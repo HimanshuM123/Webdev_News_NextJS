1992c88106af554dbde560856da6f367315667b6
unknown <himanshu.mahale2010@gmail.comm>
2022-04-27 22:42:35 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Meta from '../components/Meta'
+
+const about = () => {
+    return (
+        <diV>
+            <Meta title='About' />
+            <h1>About</h1>
+        </diV>
+    )
+}
+
+export default about
